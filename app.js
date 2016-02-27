821e5aafb6683d1537b4bb15ea0970fcd172fd54
Warren Goble <warrengoble@gmail.com>
2016-02-26 17:35:41 -0700

    Added width and height props

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import GLFrag from './glfrag'
 
-ReactDOM.render(<GLFrag/>,document.getElementById('app'))
+ReactDOM.render(<GLFrag width={window.innerWidth} height={window.innerHeight}/>,document.getElementById('app'))
